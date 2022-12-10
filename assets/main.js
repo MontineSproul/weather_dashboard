51317914f05ab58add14c1f60b9484f07fd44590
Ashley Montine Sproul <montinesproul@gmail.com>
2022-12-10 08:26:25 -0500

    got html structure completed started working on puesdo code and js

---
+++ assets/main.js
@@ -0,0 +1,26 @@
+var apiKey = '0dff05e8fe26750242f5de6308aaff1f';
+
+https://api.openweathermap.org/data/2.5/forecast?q=Atlanta&units=imperial&appid=0dff05e8fe26750242f5de6308aaff1f
+
+//moments/jquery to change the day of the week at the top of the card
+if (day is today) {
+    Today + date
+} else if (date ++) {
+    day of the week and date 
+}
+
+
+//local storage in search bar
+function searchHistory() {
+    var searchCity = document.getElementById('city').value;
+    var savedCity = localStorage.setItem("myCities", "City");
+console.log(localStorage);
+//reading the localStorage item
+var savedCities = localStorage.getItem("myCities");
+
+//add event listener for search button 
+}
+
+//function that displays the day of the week in the header
+
+//display weather in cards 
